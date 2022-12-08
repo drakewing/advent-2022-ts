7499d1160627740dda6e587de068ac0dcb6e8c93
Matt Drake <drake.mattc@gmail.com>
2022-12-07 21:07:31 -0800

    finish d04p1

--- advent.test.ts
+++ advent.test.ts
@@ -4,24 +4,24 @@ import { prepInput } from "./solutions/helpers";
 import { d01p1, d01p2 } from "./solutions/day01";
 import { d02p1, d02p2 } from "./solutions/day02";
 import { d03p1, d03p2 } from "./solutions/day03";
-// import { d04p1, d04p2 } from "./solutions/day04";
+import { d04p1, d04p2 } from "./solutions/day04";
 // import { d05p1, d05p2 } from "./solutions/day05";
 import { d06p1, d06p2 } from "./solutions/day06";
 import { d07p1, d07p2 } from "./solutions/day07";
 
-test("advent solutions", () => {
-  expect(d01p1(prepInput("input/day01.txt"))).toBe(66616);
-  expect(d01p2(prepInput("input/day01.txt"))).toBe(199172);
-  expect(d02p1(prepInput("input/day02.txt"))).toBe(10624);
-  expect(d02p2(prepInput("input/day02.txt"))).toBe(14060);
-  expect(d03p1(prepInput("input/day03.txt"))).toBe(7863);
-  expect(d03p2(prepInput("input/day03.txt"))).toBe(2488);
-  // expect(d04p1(prepInput("input/day04.txt"))).toBe(507);
-  // expect(d04p2(prepInput("input/day04.txt"))).toBe(897);
-  // expect(d05p1(prepInput("input/day05.txt"))).toBe("SPFMVDTZT");
-  // expect(d05p2(prepInput("input/day05.txt"))).toBe("ZFSJBPRFP");
-  expect(d06p1(prepInput("input/day06.txt")[0])).toBe(1275);
-  expect(d06p2(prepInput("input/day06.txt")[0])).toBe(3605);
-  expect(d07p1(prepInput("input/day07.txt"))).toBe(1118405);
-  expect(d07p2(prepInput("input/day07.txt"))).toBe(12545514);
-});
+test("d01p1", () => expect(d01p1(prepInput("input/day01.txt"))).toBe(66616));
+test("d01p2", () => expect(d01p2(prepInput("input/day01.txt"))).toBe(199172));
+test("d02p1", () => expect(d02p1(prepInput("input/day02.txt"))).toBe(10624));
+test("d02p2", () => expect(d02p2(prepInput("input/day02.txt"))).toBe(14060));
+test("d03p1", () => expect(d03p1(prepInput("input/day03.txt"))).toBe(7863));
+test("d03p2", () => expect(d03p2(prepInput("input/day03.txt"))).toBe(2488));
+test("d04p1", () => expect(d04p1(prepInput("input/day04.txt"))).toBe(507));
+test("d04p2", () => expect(d04p2(prepInput("input/day04.txt"))).toBe(897));
+// test("d05p1", () =>
+//   expect(d05p1(prepInput("input/day05.txt"))).toBe("SPFMVDTZT"));
+// test("d05p2", () =>
+//   expect(d05p2(prepInput("input/day05.txt"))).toBe("ZFSJBPRFP"));
+test("d06p1", () => expect(d06p1(prepInput("input/day06.txt")[0])).toBe(1275));
+test("d06p2", () => expect(d06p2(prepInput("input/day06.txt")[0])).toBe(3605));
+test("d07p1", () => expect(d07p1(prepInput("input/day07.txt"))).toBe(1118405));
+test("d07p2", () => expect(d07p2(prepInput("input/day07.txt"))).toBe(12545514));
---
+++ solutions/day04.ts
@@ -0,0 +1,43 @@
+interface range {
+  lower: number;
+  upper: number;
+}
+
+interface rangePair {
+  left: range;
+  right: range;
+}
+
+const splitRanges = (input: string): rangePair => {
+  const parts = input.split(",");
+  const ranges: range[] = [];
+
+  for (const part of parts) {
+    const bounds = part.split("-");
+    ranges.push({ lower: parseInt(bounds[0]), upper: parseInt(bounds[1]) });
+  }
+
+  return { left: ranges[0], right: ranges[1] }
+};
+
+// does a fully contain b?
+const isFullyContained = (a: range, b: range): boolean => {
+  return a.lower <= b.lower && a.upper >= b.upper;
+};
+
+export const d04p1 = (input: string[]): number => {
+  const pairs = input.map(str => splitRanges(str));
+  let count = 0;
+
+  for (const pair of pairs) {
+    if (isFullyContained(pair.left, pair.right) || isFullyContained(pair.right, pair.left)) {
+      count++;
+    }
+  }
+
+  return count;
+};
+
+export const d04p2 = (input: string[]): number => {
+  return 0;
+};
